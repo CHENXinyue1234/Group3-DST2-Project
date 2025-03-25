e2ae4ca4a29f7f3de6621f4815efcf47c4543a14
xinyue.23@intl.zju.edu.cn <xinyue.23@intl.zju.edu.cn>
2025-03-25 11:03:22 +0800

    3.25Jsp cxy 20250304

--- src/main/java/cn/edu/zju/servlet/DrugServlet.java
+++ src/main/java/cn/edu/zju/servlet/DrugServlet.java
@@ -17,38 +17,17 @@ import java.util.List;
 
 @WebServlet(name = "DrugServlet",  urlPatterns = "/drug")
 public class DrugServlet extends HttpServlet {
-    private static final Logger log = LoggerFactory.getLogger(DrugServlet.class);
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         DrugDao drugDao = new DrugDao();
-        List<Drug> drugs = drugDao.findAll();
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<body>");
-        out.println("<table>");
-        out.println("<thead>");
-        out.println("<tr>");
-        out.println("<th>id</th>");
-        out.println("<th>name</th>");
-        out.println("<th>obj_cls</th>");
-        out.println("<th>drug_url</th>");
-        out.println("<th>biomarker</th>");
-        out.println("</tr>");
-        out.println("</thead>");
-        out.println("<tbody>");
-        for (Drug d: drugs){
-            out.println("<tr>");
-            out.println("<td>"+d.getId()+"</td>");
-            out.println("<td>"+d.getName()+"</td>");
-            out.println("<td>"+d.getObjCls()+"</td>");
-            out.println("<td>"+d.getDrugUrl()+"</td>");
-            out.println("<td>"+d.isBiomarker()+"</td>");
-            out.println("</tr>");
-            log.info("Drug displayed: " + d.getName());
-        }
-        out.println("</tbody>");
-        out.println("</table>");
-        out.println("</body>");
-        out.println("</html>");
+        List<Drug> all = drugDao.findAll();
+
+        request.setAttribute("drugs", all);
+        request.getRequestDispatcher("/views/drugs.jsp").forward(request, response);
     }
 }
+
